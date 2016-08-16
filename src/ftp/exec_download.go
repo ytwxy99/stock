5d2447946630a55d7e0521c00cf28aaf3313fadc
root <root@tracy.tracy>
2016-08-16 16:44:09 +0800

    modify

--- src/ftp/exec_download.go
+++ src/ftp/exec_download.go
@@ -9,16 +9,16 @@ import (
         )
 
 func Run() {
-    cmd := exec.Command("ping 127.0.0.1") 
+    cmd := exec.Command("ping 127.0.0.1")
     _, err := cmd.Output()
     if err != nil {
         panic(err.Error())
     }
-                        
+
     if err := cmd.Start(); err != nil {
         panic(err.Error())
     }
-                                              
+
     if err := cmd.Wait(); err != nil {
         panic(err.Error())
     }
